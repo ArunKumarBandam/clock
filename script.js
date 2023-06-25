a580ee2bd7decd3de4e699d63bdd2048bacdef5f
ArunKumarBandam <128398258+ArunKumarBandam@users.noreply.github.com>
2023-06-25 18:30:09 +0530

    commit clock

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let time1=document.querySelector('.time');
+let pri=0;
+setInterval(()=>{
+let date = new Date();
+let h=date.getHours();
+let m=date.getMinutes();
+let s=date.getSeconds();
+let d=date.getDate();
+if(h<=12){
+   
+pri=h +':'+m+':'+s+' ';
+time1.innerHTML=pri;
+}else{
+    pri=(h-12)+'h:'+m+'m:'+s+'s ';
+    time1.textContent=pri;
+}
+
+},1000)
